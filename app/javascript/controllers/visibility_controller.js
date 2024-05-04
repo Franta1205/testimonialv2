3e8f1a67498124250cabb1a75cd4b27e10baab5c
Franta1205 <hoza.fr@gmail.com>
2024-05-04 16:03:45 +0200

    adding visibility icon to password

--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
     connect() {
         console.log("this is visibility controller")
     }
-    static targets = ["navMenu", "mobileMenu", "workspaceForm"]
+    static targets = ["navMenu", "mobileMenu", "workspaceForm", "password", "pwIcon"]
 
     toggle() {
         this.navMenuTarget.classList.toggle("hidden")
@@ -16,5 +16,16 @@ export default class extends Controller {
 
     workspaceFormToggle() {
         this.workspaceFormTarget.classList.toggle("hidden")
+    }
+
+    toggleVisibilityPassword() {
+        const pwField = this.passwordTarget;
+        const pwIcon = this.pwIconTarget;
+
+        // Toggle the type attribute of the password field
+        pwField.type = pwField.type === 'password' ? 'text' : 'password';
+
+        // Toggle the opacity of the icon
+        pwIcon.style.opacity = pwIcon.style.opacity === '0.5' ? '1' : '0.5';
     }
 }
