940d6df2373c88e198adfb6d40c31f679e132570
Franta1205 <hoza.fr@gmail.com>
2024-05-11 19:50:38 +0200

    adding side bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_locale
+  helper_method :show_index_nav?
 
   def set_locale
     I18n.locale = :cz
+  end
+
+  def show_index_nav?
+    controller_name == 'workspaces' && action_name == 'index'
   end
 end
